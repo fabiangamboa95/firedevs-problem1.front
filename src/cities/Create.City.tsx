7b1851647cd9b1ecf986e1b391e885c18e371a36
fabiangamboa95 <fabiangamboa95@gmail.com>
2021-08-25 16:44:50 -0400

    validated cities forms

--- src/cities/Create.City.tsx
+++ src/cities/Create.City.tsx
@@ -1,10 +1,11 @@
 import { Create, CreateProps, SimpleForm, TextInput } from "react-admin";
+import { requiredValidate } from "../utils/validators";
 
 const CreateCity: React.FC<CreateProps> = (props) => {
   return (
     <Create {...props} title="Agregar Ciudad">
       <SimpleForm redirect="/cities">
-        <TextInput source="nombre" />
+        <TextInput source="nombre" validate={requiredValidate} />
       </SimpleForm>
     </Create>
   );
