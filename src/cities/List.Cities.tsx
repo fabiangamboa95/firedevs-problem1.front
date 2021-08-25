68c5b3fd38177ef5562c22abda42afc38ef6b0d6
fabiangamboa95 <fabiangamboa95@gmail.com>
2021-08-25 18:22:58 -0400

    reviewed cities ux

--- src/cities/List.Cities.tsx
+++ src/cities/List.Cities.tsx
@@ -3,7 +3,7 @@ import { Datagrid, List, ListProps, TextField } from "react-admin";
 const ListCities: React.FC<ListProps> = (props) => (
   <List {...props} perPage={10} title="Ciudades">
     <Datagrid>
-      <TextField source="nombre" />
+      <TextField source="nombre" label="Nombre de la ciudad" />
     </Datagrid>
   </List>
 );
