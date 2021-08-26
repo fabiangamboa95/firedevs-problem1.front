f3405cb8e0339d4539ed8b27ce32815a5c1569b1
fabiangamboa95 <fabiangamboa95@gmail.com>
2021-08-25 23:29:31 -0400

    +friendly messages to cities forms

--- src/cities/Create.City.tsx
+++ src/cities/Create.City.tsx
@@ -1,10 +1,13 @@
 import { Create, CreateProps, SimpleForm, TextInput } from "react-admin";
 import { requiredValidate } from "../utils/validators";
 import { QFDrawerToolbar } from "../utils/QuickFormDrawer";
+import { Typography } from "@material-ui/core";
 
 const CreateCity: React.FC<CreateProps> = (props) => (
   <Create {...props} title=" ">
     <SimpleForm toolbar={<QFDrawerToolbar />}>
+      <Typography variant="h6">Agregar Ciudad</Typography>
+      <div style={{ height: 20 }}>&nbsp;</div>
       <TextInput source="nombre" autoFocus validate={requiredValidate} />
     </SimpleForm>
   </Create>
