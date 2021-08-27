02315315ed3d3029d3cfaa7fdbbbb8bc7e26d10f
fabiangamboa95 <fabiangamboa95@gmail.com>
2021-08-27 01:52:19 -0400

    fixed not refreshing on drawer initial save

--- src/utils/QuickFormDrawer.tsx
+++ src/utils/QuickFormDrawer.tsx
@@ -1,20 +1,38 @@
 import { Button, Toolbar, Drawer } from "@material-ui/core";
-import { SaveButton, CardActions, CreateButton } from "react-admin";
+import { SaveButton, CardActions, CreateButton, useRefresh } from "react-admin";
 import { useHistory } from "react-router";
+import { useStore } from "react-redux";
 
 interface QFDrawerToolbarProps {
   basePath?: string | undefined;
+  resource?: string;
 }
 
 export const QFDrawerToolbar: React.FC<QFDrawerToolbarProps> = (props) => {
   const history = useHistory();
-  const { basePath } = props;
+  const { basePath, resource } = props;
+  const refresh = useRefresh();
+  const store = useStore();
+  const {
+    admin: { resources },
+  } = store.getState();
+  const listTotal = resources[resource || ""].list.total;
 
   return (
     <Toolbar {...props}>
-      <SaveButton {...props} />
+      <SaveButton
+        {...props}
+        onSuccess={() => {
+          history.push(basePath || "/");
+          if (!listTotal) {
+            setTimeout(refresh, 500);
+          }
+        }}
+      />
       <Button
-        onClick={() => history.push(basePath || "/")}
+        onClick={() => {
+          history.push(basePath || "/");
+        }}
         style={{ marginLeft: 5 }}
       >
         Cancel
